a65b714e8161173cb96bcd8954891aef19dd30f6
Julien Huang <julien.h.dev@gmail.com>
2025-08-03 09:48:20 +0200

    refactor(runtime): move all plugins to object syntax

--- src/runtime/plugins/performance/plugin.client.ts
+++ src/runtime/plugins/performance/plugin.client.ts
@@ -23,139 +23,142 @@ function isImgElement(
   return element.tagName === 'IMG'
 }
 
-export default defineNuxtPlugin((nuxtApp) => {
-  const state = useHintIssues()
-  const issues = state.imagePerformances
+export default defineNuxtPlugin({
+  name: 'nuxt-hints:performance',
+  setup(nuxtApp) {
+    const state = useHintIssues()
+    const issues = state.imagePerformances
 
-  function findAndPushElement(element: HTMLImageElement | HTMLElement) {
-    const existingIssue = issues.value.find(issue => issue.element === element)
+    function findAndPushElement(element: HTMLImageElement | HTMLElement) {
+      const existingIssue = issues.value.find(issue => issue.element === element)
 
-    if (existingIssue) {
-      return existingIssue
+      if (existingIssue) {
+        return existingIssue
+      }
+
+      const newIssue: ImagePerformanceData = {
+        componentLocation: undefined,
+        issues: [],
+        element,
+      }
+
+      issues.value.push(newIssue)
+
+      return newIssue
     }
 
-    const newIssue: ImagePerformanceData = {
-      componentLocation: undefined,
-      issues: [],
-      element,
-    }
+    nuxtApp.hook('app:mounted', () => {
+      new PerformanceObserver((entryList) => {
+        for (const entry of entryList.getEntries()) {
+          const performanceEntry = entry
 
-    issues.value.push(newIssue)
-
-    return newIssue
-  }
-
-  nuxtApp.hook('app:mounted', () => {
-    new PerformanceObserver((entryList) => {
-      for (const entry of entryList.getEntries()) {
-        const performanceEntry = entry
-
-        console.info(
-          '[@nuxt/hints:performance] Potential LCP Element: ',
-          performanceEntry,
-        )
-
-        // If element is not an image, stop execution
-        if (!performanceEntry.element || !isImgElement(performanceEntry.element)) return
-
-        if (performanceEntry.element.attributes?.getNamedItem('loading')?.value === 'lazy') {
-          console.warn(
-            '[@nuxt/hints:performance] LCP Element should not have `loading="lazy"` \n\n Learn more: https://web.dev/optimize-lcp/#optimize-the-priority-the-resource-is-given',
+          console.info(
+            '[@nuxt/hints:performance] Potential LCP Element: ',
+            performanceEntry,
           )
-          findAndPushElement(performanceEntry.element).issues.push({
-            type: ImagePerformanceIssueType.LazyAttrOnLCPElement,
-          })
-        }
-        if (hasImageFormat(performanceEntry.element.src)) {
-          if (
-            !performanceEntry.element.src.includes('webp')
-            || !performanceEntry.element.src.includes('avif')
-          ) {
+
+          // If element is not an image, stop execution
+          if (!performanceEntry.element || !isImgElement(performanceEntry.element)) return
+
+          if (performanceEntry.element.attributes?.getNamedItem('loading')?.value === 'lazy') {
             console.warn(
-              '[@nuxt/hints:performance] LCP Element can be served in a next gen format like `webp` or `avif` \n\n Learn more: https://web.dev/choose-the-right-image-format/ \n\n Use: https://image.nuxt.com/usage/nuxt-img#format',
+              '[@nuxt/hints:performance] LCP Element should not have `loading="lazy"` \n\n Learn more: https://web.dev/optimize-lcp/#optimize-the-priority-the-resource-is-given',
             )
             findAndPushElement(performanceEntry.element).issues.push({
-              type: ImagePerformanceIssueType.ImgFormat,
+              type: ImagePerformanceIssueType.LazyAttrOnLCPElement,
+            })
+          }
+          if (hasImageFormat(performanceEntry.element.src)) {
+            if (
+              !performanceEntry.element.src.includes('webp')
+              || !performanceEntry.element.src.includes('avif')
+            ) {
+              console.warn(
+                '[@nuxt/hints:performance] LCP Element can be served in a next gen format like `webp` or `avif` \n\n Learn more: https://web.dev/choose-the-right-image-format/ \n\n Use: https://image.nuxt.com/usage/nuxt-img#format',
+              )
+              findAndPushElement(performanceEntry.element).issues.push({
+                type: ImagePerformanceIssueType.ImgFormat,
+              })
+            }
+          }
+          if (performanceEntry.element.fetchPriority !== 'high') {
+            console.warn(
+              '[@nuxt/hints:performance] LCP Element can have `fetchPriority="high"` to load as soon as possible \n\n Learn more: https://web.dev/optimize-lcp/#optimize-the-priority-the-resource-is-given',
+            )
+          }
+          if (
+            !performanceEntry.element.attributes.getNamedItem('height')
+            || !performanceEntry.element.attributes.getNamedItem('width')
+          ) {
+            console.warn(
+              '[@nuxt/hints:performance] Images should have `width` and `height` sizes set  \n\n Learn more: https://web.dev/optimize-cls/#images-without-dimensions \n\n Use: https://image.nuxt.com/usage/nuxt-img#width-height',
+            )
+            findAndPushElement(performanceEntry.element).issues.push({
+              type: ImagePerformanceIssueType.HeightWidthMissingOnLCPElement,
+            })
+          }
+          if (performanceEntry.startTime > 2500) {
+            console.warn(
+              `[@nuxt/hints:performance] LCP Element loaded in ${performanceEntry.startTime} miliseconds. Good result is below 2500 miliseconds \n\n Learn more: https://web.dev/lcp/#what-is-a-good-lcp-score`,
+            )
+            findAndPushElement(performanceEntry.element).issues.push({
+              type: ImagePerformanceIssueType.LoadingTooLong,
+            })
+          }
+
+          if (!isElementPreloaded(performanceEntry.element.src)) {
+            console.warn(
+
+              '[@nuxt/hints:performance] LCP Element can be preloaded in `head` to improve load time \n\n Learn more: https://web.dev/optimize-lcp/#optimize-when-the-resource-is-discovered \n\n Use: https://image.nuxt.com/usage/nuxt-img#preload',
+            )
+            findAndPushElement(performanceEntry.element).issues.push({
+              type: ImagePerformanceIssueType.PreloadMissingOnLCPElement,
             })
           }
         }
-        if (performanceEntry.element.fetchPriority !== 'high') {
-          console.warn(
-            '[@nuxt/hints:performance] LCP Element can have `fetchPriority="high"` to load as soon as possible \n\n Learn more: https://web.dev/optimize-lcp/#optimize-the-priority-the-resource-is-given',
+      }).observe({ type: 'largest-contentful-paint', buffered: true })
+
+      new PerformanceObserver((entryList) => {
+        for (const entry of entryList.getEntries()) {
+          const performanceEntry = entry
+
+          if (!performanceEntry.sources?.[0]) return
+
+          const sourceElement = performanceEntry.sources?.[0].node
+
+          // Nuxt DevTools button causes small layout shift so we ignore it
+          if (sourceElement.parentElement?.className.includes('nuxt-devtools')) return
+
+          console.info(
+            '[@nuxt/hints:performance] Potential CLS Element: ',
+            sourceElement,
           )
+
+          if ((performanceEntry.value ?? 0) > 0.1) {
+            console.warn(
+              `[@nuxt/hints:performance] CLS was ${performanceEntry.value}. Good result is below 0.1 \n\n Learn more: https://web.dev/articles/cls#what-is-a-good-cls-score`,
+            )
+            findAndPushElement(performanceEntry.sources?.[0].node).issues.push({
+              type: CLSIssueType.LayoutShiftTooBig,
+            })
+          }
+
+          if (
+            isImgElement(sourceElement)
+            && (!sourceElement.attributes.getNamedItem('height')
+              || !sourceElement.attributes.getNamedItem('width'))
+          ) {
+            console.warn(
+              '[@nuxt/hints:performance] Images should have `width` and `height` sizes set  \n\n Learn more: https://web.dev/optimize-cls/#images-without-dimensions \n\n Use: https://image.nuxt.com/usage/nuxt-img#width-height',
+            )
+            findAndPushElement(sourceElement).issues.push({
+              type: ImagePerformanceIssueType.HeightWidthMissingOnLCPElement,
+            })
+          }
         }
-        if (
-          !performanceEntry.element.attributes.getNamedItem('height')
-          || !performanceEntry.element.attributes.getNamedItem('width')
-        ) {
-          console.warn(
-            '[@nuxt/hints:performance] Images should have `width` and `height` sizes set  \n\n Learn more: https://web.dev/optimize-cls/#images-without-dimensions \n\n Use: https://image.nuxt.com/usage/nuxt-img#width-height',
-          )
-          findAndPushElement(performanceEntry.element).issues.push({
-            type: ImagePerformanceIssueType.HeightWidthMissingOnLCPElement,
-          })
-        }
-        if (performanceEntry.startTime > 2500) {
-          console.warn(
-            `[@nuxt/hints:performance] LCP Element loaded in ${performanceEntry.startTime} miliseconds. Good result is below 2500 miliseconds \n\n Learn more: https://web.dev/lcp/#what-is-a-good-lcp-score`,
-          )
-          findAndPushElement(performanceEntry.element).issues.push({
-            type: ImagePerformanceIssueType.LoadingTooLong,
-          })
-        }
-
-        if (!isElementPreloaded(performanceEntry.element.src)) {
-          console.warn(
-
-            '[@nuxt/hints:performance] LCP Element can be preloaded in `head` to improve load time \n\n Learn more: https://web.dev/optimize-lcp/#optimize-when-the-resource-is-discovered \n\n Use: https://image.nuxt.com/usage/nuxt-img#preload',
-          )
-          findAndPushElement(performanceEntry.element).issues.push({
-            type: ImagePerformanceIssueType.PreloadMissingOnLCPElement,
-          })
-        }
-      }
-    }).observe({ type: 'largest-contentful-paint', buffered: true })
-
-    new PerformanceObserver((entryList) => {
-      for (const entry of entryList.getEntries()) {
-        const performanceEntry = entry
-
-        if (!performanceEntry.sources?.[0]) return
-
-        const sourceElement = performanceEntry.sources?.[0].node
-
-        // Nuxt DevTools button causes small layout shift so we ignore it
-        if (sourceElement.parentElement?.className.includes('nuxt-devtools')) return
-
-        console.info(
-          '[@nuxt/hints:performance] Potential CLS Element: ',
-          sourceElement,
-        )
-
-        if ((performanceEntry.value ?? 0) > 0.1) {
-          console.warn(
-            `[@nuxt/hints:performance] CLS was ${performanceEntry.value}. Good result is below 0.1 \n\n Learn more: https://web.dev/articles/cls#what-is-a-good-cls-score`,
-          )
-          findAndPushElement(performanceEntry.sources?.[0].node).issues.push({
-            type: CLSIssueType.LayoutShiftTooBig,
-          })
-        }
-
-        if (
-          isImgElement(sourceElement)
-          && (!sourceElement.attributes.getNamedItem('height')
-            || !sourceElement.attributes.getNamedItem('width'))
-        ) {
-          console.warn(
-            '[@nuxt/hints:performance] Images should have `width` and `height` sizes set  \n\n Learn more: https://web.dev/optimize-cls/#images-without-dimensions \n\n Use: https://image.nuxt.com/usage/nuxt-img#width-height',
-          )
-          findAndPushElement(sourceElement).issues.push({
-            type: ImagePerformanceIssueType.HeightWidthMissingOnLCPElement,
-          })
-        }
-      }
-    }).observe({ type: 'layout-shift', buffered: true })
-  })
+      }).observe({ type: 'layout-shift', buffered: true })
+    })
+  },
 })
 
 const hasImageFormat = (src: string) => {
--- src/runtime/plugins/third-party-scripts/plugin.client.ts
+++ src/runtime/plugins/third-party-scripts/plugin.client.ts
@@ -1,82 +1,85 @@
 import { defineNuxtPlugin, ref, useNuxtApp } from '#imports'
 
-export default defineNuxtPlugin(() => {
-  const nuxtApp = useNuxtApp()
+export default defineNuxtPlugin({
+  name: 'nuxt-hints:third-party-scripts',
+  setup() {
+    const nuxtApp = useNuxtApp()
 
-  const scripts = nuxtApp.__hints_tpc = ref<{ element: HTMLScriptElement, loaded: boolean }[]>([])
+    const scripts = nuxtApp.__hints_tpc = ref<{ element: HTMLScriptElement, loaded: boolean }[]>([])
 
-  const isUsingNuxtScripts = !!nuxtApp.$scripts
+    const isUsingNuxtScripts = !!nuxtApp.$scripts
 
-  nuxtApp.hook('hints:scripts:added', (script: HTMLScriptElement) => {
-    scripts.value.push({ element: script, loaded: false })
-  })
+    nuxtApp.hook('hints:scripts:added', (script: HTMLScriptElement) => {
+      scripts.value.push({ element: script, loaded: false })
+    })
 
-  nuxtApp.hook('hints:scripts:loaded', (script: HTMLScriptElement) => {
-    const existingScript = scripts.value.find(s => s.element === script)
-    if (existingScript) {
-      existingScript.loaded = true
-    }
-    else {
-      console.warn(`[@nuxt/hints]: Script loaded event received for a script not tracked: ${script.src}. Please open an issue with a minimal reproduction if you think this is a bug.`)
-      scripts.value.push({ element: script, loaded: true })
-    }
-  })
-
-  nuxtApp.hooks.hookOnce('app:mounted', () => {
-    let hasThirdPartyScript = false
-    for (const script of document.scripts) {
-      if (script.src && !script.src.startsWith(window.location.origin)) {
-        hasThirdPartyScript = true
-        onScriptAdded(script)
+    nuxtApp.hook('hints:scripts:loaded', (script: HTMLScriptElement) => {
+      const existingScript = scripts.value.find(s => s.element === script)
+      if (existingScript) {
+        existingScript.loaded = true
       }
-    }
-    if (hasThirdPartyScript && !isUsingNuxtScripts) {
-      console.info('ℹ️ [@nuxt/hints]: Third-party scripts detected on page load: consider using @nuxt/scripts')
-    }
-  })
+      else {
+        console.warn(`[@nuxt/hints]: Script loaded event received for a script not tracked: ${script.src}. Please open an issue with a minimal reproduction if you think this is a bug.`)
+        scripts.value.push({ element: script, loaded: true })
+      }
+    })
 
-  // MutationObserver to catch dynamically added scripts
-  const observer = new MutationObserver((mutations: MutationRecord[]) => {
-    for (const mutation of mutations) {
-      if (mutation.type === 'childList') {
-        for (const node of mutation.addedNodes) {
-          if (isScript(node) && node.src && !node.src.startsWith(window.location.origin)) {
-            onScriptAdded(node)
+    nuxtApp.hooks.hookOnce('app:mounted', () => {
+      let hasThirdPartyScript = false
+      for (const script of document.scripts) {
+        if (script.src && !script.src.startsWith(window.location.origin)) {
+          hasThirdPartyScript = true
+          onScriptAdded(script)
+        }
+      }
+      if (hasThirdPartyScript && !isUsingNuxtScripts) {
+        console.info('ℹ️ [@nuxt/hints]: Third-party scripts detected on page load: consider using @nuxt/scripts')
+      }
+    })
+
+    // MutationObserver to catch dynamically added scripts
+    const observer = new MutationObserver((mutations: MutationRecord[]) => {
+      for (const mutation of mutations) {
+        if (mutation.type === 'childList') {
+          for (const node of mutation.addedNodes) {
+            if (isScript(node) && node.src && !node.src.startsWith(window.location.origin)) {
+              onScriptAdded(node)
+            }
           }
         }
       }
-    }
-  })
+    })
 
-  observer.observe(document.documentElement, {
-    childList: true,
-    subtree: true,
-  })
+    observer.observe(document.documentElement, {
+      childList: true,
+      subtree: true,
+    })
 
-  function onScriptAdded(script: HTMLScriptElement) {
-    if (script.src.startsWith('chrome-extension')) {
-      return
-    }
-    if (!script.crossOrigin) {
-      console.warn(`[@nuxt/hints]: Third-party script "${script.src}" is missing crossorigin attribute. Consider adding crossorigin="anonymous" for better security and error reporting.`)
-    }
-    nuxtApp.callHook('hints:scripts:added', script)
-      .then(() => {
-        if (!script.loaded) {
-          script.onload = () => {
+    function onScriptAdded(script: HTMLScriptElement) {
+      if (script.src.startsWith('chrome-extension')) {
+        return
+      }
+      if (!script.crossOrigin) {
+        console.warn(`[@nuxt/hints]: Third-party script "${script.src}" is missing crossorigin attribute. Consider adding crossorigin="anonymous" for better security and error reporting.`)
+      }
+      nuxtApp.callHook('hints:scripts:added', script)
+        .then(() => {
+          if (!script.loaded) {
+            script.onload = () => {
+              window.__hints_TPC_saveTime(script, script.__hints_TPC_start_time)
+              nuxtApp.callHook('hints:scripts:loaded', script)
+            }
+          }
+          else {
             window.__hints_TPC_saveTime(script, script.__hints_TPC_start_time)
+
             nuxtApp.callHook('hints:scripts:loaded', script)
           }
-        }
-        else {
-          window.__hints_TPC_saveTime(script, script.__hints_TPC_start_time)
+        })
 
-          nuxtApp.callHook('hints:scripts:loaded', script)
-        }
-      })
-
-    console.info(`ℹ️ [@nuxt/hints]: Dynamically added third-party script detected: ${script.src}`)
-  }
+      console.info(`ℹ️ [@nuxt/hints]: Dynamically added third-party script detected: ${script.src}`)
+    }
+  },
 })
 
 function isScript(node: Node): node is HTMLScriptElement {
